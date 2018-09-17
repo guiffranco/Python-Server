4b98c55451822691f46518cf2e0e3085b923fc03
Guilherme Franco <guiffranco1@gmail.com>
2018-09-17 01:21:05 -0300

    Remove nonsense loop

--- cli.py
+++ cli.py
@@ -1,4 +1,5 @@
 import socket
+import datetime
 
 # cria um socket usando o protocolo IPV4 e conexão TCP
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -6,26 +7,27 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = "" #127.0.0.1
 port = 7000
 
-msg = "msg from cli to serv"
+msg3 = ""
+msg = "msg from cli to server"
+msg2 = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
 
 # conecta com o servidor
 s.connect((host, port))
 
 try:
-    print('Sendind message...')
-    s.sendall(msg.encode('ascii'))
-
+    print("--------------------------")
+    print(datetime.datetime.now())
+    print("Sendind message...")
+    s.sendall(msg.encode("ascii"))
     # aguardando a resposta
-    amount_recv = 0
-    amount_expc = len(msg)
-    # passa 1024 bytes pela rede
-    while amount_recv < amount_expc:
-        data = s.recv(100)
-        amount_recv += len(data)
-        # mostrar dados
-        print('Data received: ', data.decode("ascii"))
-        print('Data size: ', amount_recv)
+
+    data = s.recv(1024)
+
+    # mostrar dados
+    print("Data received:", data.decode("ascii"))
+
+    print("Block data size:", len(data), "bytes")
 
 finally:
-    print('Closing socket...')
+    print("Closing socket...")
     s.close()
--- serv.py
+++ serv.py
@@ -1,39 +1,41 @@
 import socket
+import datetime
 
 # cria um socket usando o protocolo IPV4 e conexão TCP
-
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host = "127.0.0.1"
 port = 7000
 
 # mensagem para quando se conectar
-
-msg = b"this is a msg from serv to cli"
+msg = b"hey, this is a msg from serv to cli"
 
 # ativar o servidor/servidor em escuta
-
 s.bind((host, port))
 s.listen(10)
 
 # deixar o servidor escutando a todo momento
-
 while True:
 	# espera a conexão
 	con, add = s.accept()
 	try:
-		print("-------------------------")
-		print("Conectado com ", add)
-
-		data = con.recv(100)
-		print('Data received: ', data.decode("ascii"))
-		print('Data size: ', len(data))
+		print("--------------------------")
+		print(datetime.datetime.now())
+		print("Client connected:", add)
+		buff_size = 1024
+		data = con.recv(buff_size)
+		print("I can only handle", buff_size, "bytes")
+		print("Data received:", data.decode("ascii"))
+		print("Data size:", len(data), "bytes")
 		if data:
-			print('Sendind data to client...')
+			print("Sendind data to client...")
 			con.send(msg)
-			con.close()
+			print("Done!")
 		else:
-			print("No more data from ", add)
+			print("No more data from", add)
 			break
 	finally:
-		con.close() # fecha a conexão
+		# fecha a conexão
+		print("Closing my client socket...")
+		con.close()
+	print("I'm not dead... I'll keep listening...")
